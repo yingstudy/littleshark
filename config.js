95120e3bafe37af9d213c4f688f297b5061bfa26
yingstudy <qiyingggc@gmail.com>
2021-08-26 21:07:05 +0700

    init

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Hee luu con cá của anh!',
+    introDesc: `Giờ anh hỏi em cái này, nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'oke',
+    title: 'Em thích anh có phải không 😙',
+    desc: 'Em không trả lời hoặc thoát ra, tức là em là bạn gái anh đó nha :> ',
+    btnYes: 'yêu ơi là iuuu <33',
+    btnNo: 'anh mơ à :333',
+    question:'Vì sao em yêu anh á <3',
+    btnReply: 'Gửi cho anh <3',
+    mess: 'Anh biết mà 🥰. Love you 300.000 😘😘',
+    messDesc: 'còn giờ thì chờ gì nữa mà không inbox cho anh đi nào.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/Nhongver2',
+    reply: 'Thì.. thích thì thích thế thôi'
+}
